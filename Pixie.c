15e6d76650733ebef8f4cbf5eded5ebbaf177d56
himran <hmdimranbiswas@gmail.com>
2018-10-10 22:08:40 +0600

    Add files via upload

---
+++ Pixie.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include<math.h>
+int main(){
+    double n,nifeSize,R;
+    int T,i;
+    scanf("%d",&T);
+    if(T<=100){
+        for(i=1;i<=T;i++){
+        scanf("%lf",&n);
+        if(n<=1000){
+            R=n/(2*M_PI);
+            nifeSize=R*2;
+            printf("Case %d: %.3f\n",i,nifeSize);
+        }
+    }
+    }
+    return 0;
+}
